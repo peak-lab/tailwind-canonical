968e845415332a38001b0f9420785f24d784159d
Fahari <mkl.devops@gmail.com>
2026-07-04 17:32:22 +0100

    chore: convert config example to typescript

---
+++ tailwind-canonical.config.example.ts
@@ -0,0 +1,11 @@
+import type { Config } from 'tailwind-canonical'
+
+export default {
+  customTextTokens: {
+    10: '3xs',
+    11: '2xs',
+    13: 'xxs',
+  },
+  customSpacingTokens: {},
+  ignorePatterns: [],
+} satisfies Config
